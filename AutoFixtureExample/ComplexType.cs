cf4811254e9745a633407bbb90eb3a24c97ff005
Jack Histon <jrhiston@gmail.com>
2017-11-01 19:44:03 +0000

    Initial commit

---
+++ AutoFixtureExample/ComplexType.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoFixtureExample
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString() => $"Id: {Id}, Name: {Name}";
+    }
+}
---
+++ AutoFixtureExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+
+namespace AutoFixtureExample
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var fixture = new Fixture();
+
+            // Creating strings
+            var str = fixture.Create<string>();
+
+            Console.WriteLine(str);
+
+            // Creating numbers
+            var num = fixture.Create<decimal>();
+
+            Console.WriteLine(num);
+
+            // Creating complex types
+            var person = fixture.Create<Person>();
+
+            Console.WriteLine(person);
+
+            // Manipulating lists
+            var list = new List<Person>();
+            fixture.AddManyTo(list);
+
+            Console.WriteLine(string.Join($"PERSON: {Environment.NewLine}", list));
+        }
+    }
+}
